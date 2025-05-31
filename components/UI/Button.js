41a915c1bf85f8cae15d92bdecb5fa4beefcd638
sam2829 <samscarisbrick@outlook.com>
2025-05-31 15:17:07 +0100

    Add ability for users to add the player names and also reset all group data

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -24,6 +24,7 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 16,
     borderRadius: 8,
+    minWidth: 110,
   },
   buttonText: {
     textAlign: "center",
--- screens/GroupsScreen.js
+++ screens/GroupsScreen.js
@@ -3,6 +3,7 @@ import { Colors } from "../constants/styles";
 import Header from "../components/UI/Header";
 import GroupNumbers from "../components/groups/GroupNumbers";
 import { useState } from "react";
+import Button from "../components/UI/Button";
 
 // component to render group screen
 function GroupScreen() {
@@ -12,17 +13,37 @@ function GroupScreen() {
   //   use state hook for number of Groups
   const [numberOfGroups, setNumberOfGroups] = useState();
 
+  //   use state hook for player names
+  const [playerNames, setPlayerNames] = useState({});
+
   //   function for handling number of players
   const handlePlayerNumbers = (value) => {
-    const cleanedValue = value.replace(/[^1-9]/g, "");
+    const cleanedValue = value.replace(/[^0-9]/g, "");
     setNumberOfPlayers(value ? Number(cleanedValue) : 0);
   };
 
   //   function for handling number of groups
   const handleNumberOfGroups = (value) => {
-    const cleanedValue = value.replace(/[^1-9]/g, "");
+    const cleanedValue = value.replace(/[^0-9]/g, "");
     setNumberOfGroups(value ? Number(cleanedValue) : 0);
   };
+
+  //   function for handling player names
+  const handleChangePlayerNames = (index, name) => {
+    setPlayerNames((prev) => ({
+      ...prev,
+      [`player${index + 1}`]: name,
+    }));
+  };
+
+  //   function to handle resetting all data
+  const handleResetGroupdata = () => {
+    setNumberOfPlayers("");
+    setNumberOfGroups("");
+    setPlayerNames({});
+  };
+
+  console.log(playerNames);
 
   return (
     <ScrollView style={styles.container}>
@@ -35,11 +56,44 @@ function GroupScreen() {
         numberOfGroups={numberOfGroups}
         onGroupNumberChange={handleNumberOfGroups}
       />
-      {numberOfPlayers > 0 &&
-        numberOfGroups > 0 &&
-        Array.from({ length: numberOfPlayers }, (_, i) => (
-          <Text key={i}>Player {i + 1}</Text>
-        ))}
+      <View style={styles.playerNamesContainer}>
+        {numberOfPlayers > 0 &&
+          numberOfGroups > 0 &&
+          Array.from({ length: numberOfPlayers }, (_, i) => (
+            <View key={i} style={styles.playerBlock}>
+              <Text style={styles.player}>Player {i + 1}:</Text>
+              <TextInput
+                keyboardType="default"
+                style={styles.playerNameInput}
+                placeholder={`Enter player name`}
+                value={playerNames[`player${i + 1}`] || ""}
+                onChangeText={(text) => handleChangePlayerNames(i, text)}
+              />
+            </View>
+          ))
+            .reduce((rows, element, index) => {
+              if (index % 2 === 0) {
+                rows.push([element]);
+              } else {
+                rows[rows.length - 1].push(element);
+              }
+              return rows;
+            }, [])
+            .map((row, i) => (
+              <View key={i} style={styles.row}>
+                {row}
+                {/* Filler to keep layout aligned */}
+                {row.length === 1 && <View style={styles.playerBlock} />}
+              </View>
+            ))}
+      </View>
+      {/* import and display reset and submit player buttons */}
+      <View style={styles.buttonsContainer}>
+        <Button onPress={handleResetGroupdata} style={styles.button}>
+          Reset
+        </Button>
+        <Button style={styles.button}>Randomise</Button>
+      </View>
     </ScrollView>
   );
 }
@@ -52,5 +106,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.colors.lightGrey,
     paddingHorizontal: 10,
+  },
+  playerNamesContainer: {},
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  playerBlock: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginBottom: 15,
+  },
+  player: {
+    fontSize: 14,
+    fontWeight: "500",
+    marginBottom: 8,
+  },
+  playerNameInput: {
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: Colors.colors.primaryGold,
+    fontSize: 12,
+    padding: 10,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    marginVertical: 20,
   },
 });
